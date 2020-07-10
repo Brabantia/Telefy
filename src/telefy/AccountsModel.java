280a25c3ab7676a20caa545ba99246e6ec878143
Brabantia <45050586+Brabantia@users.noreply.github.com>
2020-07-10 02:17:26 -0700

    Delete AccountsModel.java

--- src/telefy/AccountsModel.java
+++
@@ -1,8 +0,0 @@
-package telefy;
-
-public interface AccountsModel {
-	public abstract Account[] getAllAccounts();
-	public abstract Account getAccount(int id);
-	public abstract Account getLogin(int id);
-	public abstract Account getLogin(String email);
-}
