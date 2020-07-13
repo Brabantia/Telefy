63bc81ef93162ad3dcebbf2ce1aca157de7d1a22
yorickvandewater@192.168.86.246 <>
2020-07-13 10:01:44 -0700

    Remove logging for file requests

--- src/telefy/controller/FileHandler.java
+++ src/telefy/controller/FileHandler.java
@@ -1,5 +1,7 @@
 package telefy.controller;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import telefy.model.ResourceModel;
 import telefy.entity.Resource;
 import telefy.view.FileView;
@@ -7,7 +9,7 @@ import java.io.IOException;
 import telefy.HttpRequest;
 import telefy.HttpResponse;
 
-public class FileHandler extends SafeHttpHandler {
+public class FileHandler implements HttpHandler {
 	private final ResourceModel resourceModel;
 
 	public FileHandler(ResourceModel resourdeModel) {
@@ -15,7 +17,10 @@ public class FileHandler extends SafeHttpHandler {
 	}
 
 	@Override
-	public void handle(HttpRequest req, HttpResponse resp) throws IOException {
+	public void handle(HttpExchange exchange) throws IOException {
+		HttpRequest req = new HttpRequest(exchange);
+		HttpResponse resp = new HttpResponse(exchange);
+
 		Resource resource = this.resourceModel.get(req.getPath());
 		if (resource == null) {
 			resp.respondNotFound();
